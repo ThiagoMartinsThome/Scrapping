28637d37a263d85ba8f9e95cfe5e619aa4d03251
gonzalo71 <gonzalofuste@gmail.com>
2020-10-20 22:04:41 +0200

    "añado archivo py de prueba"

---
+++ prueba.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# creo una simple grafica de prueba
+X= np.random.randn(50)
+Y= np.random.randn(50)
+
+plt.scatter(X,Y,color="r")
+plt.title("Grafica de puntos")
+plt.show()
