67c6753d2ea3b908c813ef2bb4ce2be9abb65f6b
gonzalo71 <gonzalofuste@gmail.com>
2020-11-02 19:52:54 +0100

    he creado un branch y hecho una funcion; borre los prints

--- prueba.py
+++ prueba.py
@@ -3,24 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# creo una simple grafica de prueba
-X= np.random.randn(50)
-Y= np.random.randn(50)
+def color_grafica():
+    # pregunto usuario color grafica
+    pregunta = input("que color quieres usar: r,b,o,g?")
 
-plt.scatter(X,Y,color="r")
-plt.title("Grafica de puntos")
-plt.show()
 
-# prueba prueba#
-print("hola")
-print("hola de nuevo")
+    # creo una simple grafica de prueba
+    X= np.random.randn(50)
+    Y= np.random.randn(50)
 
-# segunad prueba#
-print ("holta")
+    plt.scatter(X,Y,color=pregunta)
+    plt.title("Grafica de puntos")
+    plt.show()
 
-## Thiago Cambio#
-print("Thiago")
+#pregunto de nuevo
+pregunta_fin = input("quieres ver la grafica Y/N")
+try:
+    if pregunta_fin == "Y":
+        color_grafica()
+    elif pregunta_fin == "N":
+        print ("gracias por usar el programma")
+except:
+        print("An exception occurred")   
 
-print("hola dos ")
-
-print("Gonzalo2")
